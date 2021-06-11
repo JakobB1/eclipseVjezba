30f52ea4bdc10236c022cb09243f66b49da7277d
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-11 17:52:45 +0100

    Edunova03vjezbaZadaci

--- Edunova03/src/edunova/Zadaci.java
+++ Edunova03/src/edunova/Zadaci.java
@@ -5,25 +5,71 @@ import javax.swing.JOptionPane;
 public class Zadaci {
 	
 	public static void main(String[] args) {
-		// deklarirajte varijablu za pohranu tjelesne
-		// temperature èovjeka kao int i dodjelite 
-		// vrijednost 36
 		
+		// zadatak 1
+		// deklarirajte varijablu za pohranu tjelesne
+		// temperature covjeka kao int i dodjelite 
+		// vrijednost 36
+		int tjelesnaTemperatura=36;
+		
+		System.out.println(tjelesnaTemperatura);
+		
+		// zadatak 2
 		// deklarirajte varijablu za temperaturu
 		// mjesta kao int i dodjelite 
-		// vrijednost -9. Ispišite deklariranu vrijednost
+		// vrijednost -9. Ispisite deklariranu vrijednost
+		int temperaturaMjesta=-9;
+		System.out.println(temperaturaMjesta);
 		
-		
-		// Korisnik uèitava dva cijela broja
+		// zadatak 3
+		// Korisnik ucitava dva cijela broja
 		// prpgram ispisuje njivog zbroj
+		int prviBroj, drugiBroj, zbroj;
 		
+		prviBroj = Integer.parseInt(
+				JOptionPane.showInputDialog("Upiši prvi broj")
+				);
 		
-		// Program od korisnika uèitava tri broja
-		// program ispisuje zbroj prvog i treæeg
+		drugiBroj = Integer.parseInt(
+				JOptionPane.showInputDialog("Upiši drugi broj")
+				);
+		
+		zbroj = prviBroj + drugiBroj;
+		
+		System.out.println("Zbroj brojeva je " + zbroj);
+		
+		// zadatak 4
+		// Program od korisnika ucitava tri broja
+		// program ispisuje zbroj prvog i treceg
 		// podjeljen s drugim unesenim brojem
 		
+		int a, b, c;
+
+		a = Integer.parseInt(
+				JOptionPane.showInputDialog("Upiši prvi broj")
+				);
+
+		b = Integer.parseInt(
+				JOptionPane.showInputDialog("Upiši drugi broj")
+				);
+
+		c = Integer.parseInt(
+				JOptionPane.showInputDialog("Upiši treci broj")
+				);
+
+		float zbroj1 = (float) (a + c) / b;
+
+		System.out.println("Zbroj prvog i treceg broja podijeljen sa drugim brojem je " + zbroj1);
+		
+		// zadatak 5
 		// Korisnik unosi dvoznamenkasti broj
 		// Program ispisuje prvu znamenku
+		
+		int broj;
+
+		broj = Integer.parseInt(JOptionPane.showInputDialog("Unesi dvoznamenkasti broj"));
+
+		System.out.println("Prva znamenka je " + (broj / 10));
 		
 		
 	}
