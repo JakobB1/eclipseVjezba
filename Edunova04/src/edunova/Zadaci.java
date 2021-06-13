c4af92cdf543237b5a4f25614c07d1f3580e5249
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-13 22:47:05 +0100

    vjezba

--- Edunova04/src/edunova/Zadaci.java
+++ Edunova04/src/edunova/Zadaci.java
@@ -3,61 +3,28 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Zadaci {
+	
+	public static void main(String[] args) {
+		
 
 	// Zadatak 1
 	// korisnik unosi cijeli broj
 	// za parni broj se ispisuje Osijek
 	// za neparni broj se ispisuje Donji Miholjac
 	
-	// dodatno zadatak 1
-	// U sluèaju unosa parnog broja tražiti unos
-	// dva cijela broja i ispisati zbroj
+	int a;
 	
-	//u sluèaju unosa neparnog broja tražiti unos
-	// decimalnog broja i ispisati samo cijeli dio
-
+	a = Integer.parseInt(
+			JOptionPane.showInputDialog("Upiši cijeli broj")
+			);
 	
-	
-	
-	// Zadatak 2
-	// Program unosi dva cijela broja od korisnika
-	// program ispisuje veæi uneseni broj
-	
-	// jedno od rješenja
-	/*
-	public static void main(String[] args) {
-		if(Integer.parseInt(
-				JOptionPane.showInputDialog("Prvi broj"))>
-		Integer.parseInt(JOptionPane.showInputDialog("Drugi broj"))) {
-			System.out.println("Prvi je veæi");
-		}else {
-			System.out.println("Drugi je veæi");
-		}
+	if(a% 2==0) {
+		System.out.println("Broj je paran");
+	}else {
+		System.out.println("Broj je neparan");
 	}
-	*/
-	
-	// Zadatak 3
-	// Program unosi tri cijela broja od korisnika
-	// program ispisuje najmanji uneseni broj
-	
-	// Zadatak 4 - primitivni kalkulator
-	// Korisnika unosi prvo operaciju:
-	// 1 - zbrajanje
-	// 2 - oduzimanje
-	// 3 - množenje
-	// 4 - djeljenje
-	// za sve ostale unose program ispisuje: GREŠKA i završava se
-	// Korisnik unosi dva decimalna broja
-	// U odnosu na odabranu operaciju program ispisuje rezultat
-	// Ako je rezultat cijeli broj tada se ispisuje bez .0
-	
-	
-	//
-	// Zadatak 5
-	// Program unosi dva cijela broja.
-	// U sluèaju da je zbroj unesenih brojeva veæi od 10 
-	// i razlika prvog i drugog manja od 0 program ispisuje
-	// Osijek, inaèe ispisuje Donji Miholjac.
+
+}	
 }
 
 
