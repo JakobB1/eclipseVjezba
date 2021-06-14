89ddfc76526545c9fbe846ebbf187b37aaa9a10b
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-14 11:02:32 +0100

    zadaciVjezba

--- Edunova04/src/edunova/Zadaci.java
+++ Edunova04/src/edunova/Zadaci.java
@@ -36,33 +36,82 @@ public class Zadaci {
 		} else {
 			System.out.println(c);
 		}
-		
+
 		// Zadatak 3
 		// Program unosi tri cijela broja od korisnika
 		// program ispisuje najmanji uneseni broj
-		
-		int d,e,f;
-		
-		d = Integer.parseInt(
-				JOptionPane.showInputDialog("Upisi prvi broj")
-				);
-		
-		e = Integer.parseInt(
-				JOptionPane.showInputDialog("Upisi drugi broj")
-				);
-		
-		f = Integer.parseInt(
-				JOptionPane.showInputDialog("Upisi treci broj")
-				);
-		 
-		if (d<e && d<f) { 
+
+		int d, e, f;
+
+		d = Integer.parseInt(JOptionPane.showInputDialog("Upisi prvi broj"));
+
+		e = Integer.parseInt(JOptionPane.showInputDialog("Upisi drugi broj"));
+
+		f = Integer.parseInt(JOptionPane.showInputDialog("Upisi treci broj"));
+
+		if (d < e && d < f) {
 			System.out.println(d);
-		}else if (e<f && e<f) {
+		} else if (e < f && e < f) {
 			System.out.println(e);
 		} else {
 			System.out.println(f);
 		}
-		
 
+		// Zadatak 4 - primitivni kalkulator
+		// Korisnika unosi prvo operaciju:
+		// 1 - zbrajanje
+		// 2 - oduzimanje
+		// 3 - množenje
+		// 4 - djeljenje
+		// za sve ostale unose program ispisuje: GREŠKA i završava se
+		// Korisnik unosi dva decimalna broja
+		// U odnosu na odabranu operaciju program ispisuje rezultat
+		// Ako je rezultat cijeli broj tada se ispisuje bez .0
+
+		int op;
+		float zbrajanje;
+		float oduzimanje;
+		float mnozenje;
+		float dijeljenje;
+		float br1;
+		float br2;
+
+		br1 = Float.parseFloat(JOptionPane.showInputDialog("Unos prvog decimalnog broja"));
+
+		br2 = Float.parseFloat(JOptionPane.showInputDialog("Unos drugog decimalnog broja"));
+
+		op = Integer.parseInt(JOptionPane.showInputDialog("Unos operacije"));
+
+		zbrajanje = br1 + br2;
+		oduzimanje = br1 - br2;
+		mnozenje = br1 * br2;
+		dijeljenje = br1 / br2;
+
+		if (op == 1) {
+			if (zbrajanje == (int) zbrajanje)
+				;
+			System.out.println("zbrajanje");
+
+		} else if (op == 2) {
+			if (oduzimanje == (int) oduzimanje)
+				;
+			System.out.println("oduzimanje");
+
+		} else if (op == 3) {
+			if (mnozenje == (int) mnozenje)
+				;
+			System.out.println("mnozenje");
+
+		} else if (op == 4) {
+			if (dijeljenje == (int) dijeljenje)
+				;
+			System.out.println("dijeljenje");
+
+		}
+
+		if (op < 1 || op > 4) {
+			System.out.println("GRESKA");
+
+		}
 	}
 }
