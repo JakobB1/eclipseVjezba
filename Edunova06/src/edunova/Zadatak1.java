2b5f1cf0d48261c3bd8cd91d2abf78be1a5294ce
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-18 16:27:48 +0100

    zadatak1

--- Edunova06/src/edunova/Zadatak1.java
+++ Edunova06/src/edunova/Zadatak1.java
@@ -1,9 +1,20 @@
 package edunova;
 
+import javax.swing.JOptionPane;
 
 public class Zadatak1 {
-	
-	//Program unosi 2 broja i ispisuje zbroj
-	
-	
+
+	public static void main(String[] args) {
+
+		// Program unosi 2 broja i ispisuje zbroj
+
+		int a, b, c;
+		a = Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli broj"));
+		b = Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli broj"));
+
+		c = a + b;
+
+		System.out.println(c);
+
+	}
 }
