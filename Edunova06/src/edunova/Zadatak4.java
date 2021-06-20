afd1e2c72aabf25360a4162e35f8cd503927ca8e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-20 11:11:58 +0100

    zadatak4

--- Edunova06/src/edunova/Zadatak4.java
+++ Edunova06/src/edunova/Zadatak4.java
@@ -3,16 +3,20 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Zadatak4 {
-	
+
 	public static void main(String[] args) {
-		
-	
-	
-	// Program uèitava cijele brojeve od korisnika
-	// sve dok se ne uèita broj 0. Tada se program završava
-	
-	
-}
-}
 
+		// Program uèitava cijele brojeve od korisnika
+		// sve dok se ne uèita broj 0. Tada se program završava
 
+		int x;
+		for (;;) {
+			x = Integer
+					.parseInt(JOptionPane.showInputDialog("daj mi jedan broj,ako uneses nula" + ",program zavrsava"));
+
+			if (x == 0) {
+				break;
+			}
+		}
+	}
+}
