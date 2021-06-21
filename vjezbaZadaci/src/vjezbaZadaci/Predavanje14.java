13e9a66191bd76573e09f709634b9bc07fe8d51f
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-21 11:18:00 +0100

    vjezbanje

---
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje14.java
@@ -0,0 +1,11 @@
+package vjezbaZadaci;
+
+public class Predavanje14 {
+
+	public static void main(String[] args) {
+
+		// Program uèitava 5 decimalnih brojeva u niz
+		// Program ispisuje prvi i posljednji element niza
+
+	}
+}
