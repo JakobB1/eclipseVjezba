0dfefd2bfce519e6ccd9760f57b51e752ba94179
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-21 13:24:20 +0100

    vjezba

--- vjezbaZadaci/src/vjezbaZadaci/Predavanje14.java
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje14.java
@@ -1,5 +1,7 @@
 package vjezbaZadaci;
 
+import javax.swing.JOptionPane;
+
 public class Predavanje14 {
 
 	public static void main(String[] args) {
@@ -7,5 +9,23 @@ public class Predavanje14 {
 		// Program uèitava 5 decimalnih brojeva u niz
 		// Program ispisuje prvi i posljednji element niza
 
+		float[] dec = new float[5];
+
+		dec[0] = Float.parseFloat(JOptionPane.showInputDialog("Unesi prvi"));
+		dec[1] = Float.parseFloat(JOptionPane.showInputDialog("Unesi drugi"));
+		dec[2] = Float.parseFloat(JOptionPane.showInputDialog("Unesi treci"));
+		dec[3] = Float.parseFloat(JOptionPane.showInputDialog("Unesi cetvrti"));
+		dec[4] = Float.parseFloat(JOptionPane.showInputDialog("Unesi peti"));
+
+		System.out.println("Prvi br je " + dec[0] + " a zadnji " + dec[4]);
+
+		// Drugi nacin
+
+		double[] b = { Double.parseDouble(JOptionPane.showInputDialog("B1")),
+				Double.parseDouble(JOptionPane.showInputDialog("B2")),
+				Double.parseDouble(JOptionPane.showInputDialog("B3")),
+				Double.parseDouble(JOptionPane.showInputDialog("B4")),
+				Double.parseDouble(JOptionPane.showInputDialog("B5")) };
+		System.out.println(b[0] + " - " + b[4]);		
 	}
 }
