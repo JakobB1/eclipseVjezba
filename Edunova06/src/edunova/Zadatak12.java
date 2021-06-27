9b0dd7c8790275ce58a6ad500110fd65fc4c1fac
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-27 18:31:18 +0100

    zadatak08

--- Edunova06/src/edunova/Zadatak12.java
+++ Edunova06/src/edunova/Zadatak12.java
@@ -2,6 +2,10 @@ package edunova;
 
 public class Zadatak12 {
 	
+	public static void main(String[] args) {
+		
 	// Program ispisuje "Edunova" (ispisuju se i navodnici)
 
+	System.out.println("\"Edunova\"");	
+}
 }
