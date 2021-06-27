ac4b3b42e05c01d6a3094e10232e9282d9864941
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-27 18:31:06 +0100

    zadatak07

--- Edunova06/src/edunova/Zadatak07.java
+++ Edunova06/src/edunova/Zadatak07.java
@@ -1,11 +1,29 @@
 package edunova;
 
 public class Zadatak07 {
-
+	
+	public static void main(String[] args) {
+		
 	// Prim broj je broj koji je cjelobrojno djeljiv sam s sobom
 	// i s broj 1.
 	// Za uneseni broj x = 27353 ispišite da li je prim broj
+		
+	int x = 273532542;
+	boolean primBroj = true;
+	for (int i = 2; i<x ; i++) {
+		
+		if (x % i ==0) {
+			primBroj = false;
+			break;
+		}
+	}
+	if (primBroj) {
+		System.out.println("Broj je prim");
+	} else {
+		System.out.println("Broj nije prim");
+	}
 
 	
 
+}
 }
