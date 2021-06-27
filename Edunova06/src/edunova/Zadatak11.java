5fa3e64a1d0e53aab5ce0c7e90c63be4231d02ac
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-27 18:32:36 +0100

    zadatak11

--- Edunova06/src/edunova/Zadatak11.java
+++ Edunova06/src/edunova/Zadatak11.java
@@ -2,7 +2,17 @@ package edunova;
 
 public class Zadatak11 {
 	
+	public static void main(String[] args) {
+	
 	// Za dva dana broja ispišite decimalni dio rezultata
 	// djeljenja
-
+		
+	double a=3, b=2;
+	
+	double rez = a/b;
+	
+	int r = (int) rez;
+	
+	System.out.println(rez - r);
+}
 }
