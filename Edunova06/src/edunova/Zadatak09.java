2f4b34c87c64732d2ffe9c97c939922287e2c04a
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-27 18:32:08 +0100

    zadatak09

--- Edunova06/src/edunova/Zadatak09.java
+++ Edunova06/src/edunova/Zadatak09.java
@@ -4,9 +4,22 @@ import javax.swing.JOptionPane;
 
 public class Zadatak09 {
 	
+	public static void main(String[] args) {
+		
 	//Program od korisnika unosi brojeve
 	// sve dok korisnik ne unese broj 0
 	//Program ispisuje zbroj svih unesenih brojeva
 	
+	int a;
+	int sum = 0;
 	
+	for(;;) {
+		a = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		if ( a == 0) {
+			break;
+		}
+		sum=sum+a;
+	}
+	System.out.println(sum);
+}
 }
