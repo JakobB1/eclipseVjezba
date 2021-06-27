f64fbd3c38b2bfc45383b7cf9202d5599a38f9c2
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-27 18:31:55 +0100

    zadatak08

--- Edunova06/src/edunova/Zadatak08.java
+++ Edunova06/src/edunova/Zadatak08.java
@@ -2,7 +2,20 @@ package edunova;
 
 public class Zadatak08 {
 	
+	public static void main(String[] args) {
+		
 	// Program ispisuje matricu 15 x 15
 	// tako da je svaki element matrice broj 0
+		
+	int a=15, b=15;
 	
-}
+	for (int i=0; i<a; i++) {
+		for (int j=0; j<b; j++) {
+			System.out.print(0);
+		}
+		System.out.println();
+	}
+		
+	
+	}
+}	
