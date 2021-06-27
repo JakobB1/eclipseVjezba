a42029e223798a255240735b5dfc309b2b966e82
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-27 18:26:14 +0100

    zadatak05

--- Edunova06/src/edunova/Zadatak05.java
+++ Edunova06/src/edunova/Zadatak05.java
@@ -2,7 +2,7 @@ package edunova;
 
 import javax.swing.JOptionPane;
 
-public class Zadatak5 {
+public class Zadatak05 {
 
 	// Program od korisnika unosi dva parna broja
 	// Program ispisuje njohov zbroj
