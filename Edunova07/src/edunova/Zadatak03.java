3877112ae9bb3768c215c241657b76aa1609dfad
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-28 16:14:18 +0100

    zadatak03

---
+++ Edunova07/src/edunova/Zadatak03.java
@@ -0,0 +1,26 @@
+package edunova;
+
+public class Zadatak03 {
+	
+	public static void main(String[] args) {
+				
+		// Ulaz u program je niz znakova koji sadrži
+		// imena gradova razdvojeno zarezom
+		// Program ispisuje naziv grada koji u sebi ima najmanje znakova
+		
+		// Primjer ulaza: "Osijek, Zagreb, Varaždin, Sinj, Donji Miholjac"
+		// Sinj 
+		
+		String s = "Osijek, Zagreb , Varaždin, Sinj, Donji Miholjac";
+		String[] gradovi = s.split(",");
+		int najmanji = Integer.MAX_VALUE;
+		int index=0;
+		for(int i=0; i<gradovi.length; i++) {
+			if(gradovi[i].trim().length()<najmanji) {
+				najmanji = gradovi[i].trim().length();
+			}
+			index=i;
+		}
+	}
+
+}
