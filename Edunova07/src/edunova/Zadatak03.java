23625af7d16dfceb99ad6a6b5d105bbd6ee1680b
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-28 16:15:52 +0100

    zadatak03

--- Edunova07/src/edunova/Zadatak03.java
+++ Edunova07/src/edunova/Zadatak03.java
@@ -1,26 +1,33 @@
 package edunova;
 
+import java.util.Arrays;
+
 public class Zadatak03 {
 	
+	// Ulaz u program je niz znakova koji sadrži
+	// imena gradova razdvojeno zarezom
+	// Program ispisuje naziv grada koji u sebi ima najmanje znakova
+
+	//Primjer ulaza: "Osijek, Zagreb, Varaždin, Sinj, Donji Miholjac"
+	// Sinj
+	
 	public static void main(String[] args) {
-				
-		// Ulaz u program je niz znakova koji sadrži
-		// imena gradova razdvojeno zarezom
-		// Program ispisuje naziv grada koji u sebi ima najmanje znakova
-		
-		// Primjer ulaza: "Osijek, Zagreb, Varaždin, Sinj, Donji Miholjac"
-		// Sinj 
-		
-		String s = "Osijek, Zagreb , Varaždin, Sinj, Donji Miholjac";
+		String s = "Osijek, Zagreb, Varaždin, Sinjski lug, Donji Miholjac";
 		String[] gradovi = s.split(",");
-		int najmanji = Integer.MAX_VALUE;
+		int najmanji=Integer.MAX_VALUE;
 		int index=0;
-		for(int i=0; i<gradovi.length; i++) {
+		for(int i=0;i<gradovi.length;i++) {
 			if(gradovi[i].trim().length()<najmanji) {
 				najmanji = gradovi[i].trim().length();
+				index=i;
 			}
-			index=i;
 		}
+		System.out.println(gradovi[index].trim().toUpperCase());
+		for(String grad: gradovi) {
+			if(grad.trim().length()==najmanji) {
+				System.out.println(grad.trim().toLowerCase());
+			}
+		}
+		
 	}
-
 }
