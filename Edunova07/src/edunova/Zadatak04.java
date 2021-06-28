56fceaf50c554d32c07875dd862dbdaa936059a2
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-28 16:42:13 +0100

    zadatak04

---
+++ Edunova07/src/edunova/Zadatak04.java
@@ -0,0 +1,20 @@
+package edunova;
+
+public class Zadatak04 {
+
+	public static void main(String[] args) {
+
+		// Ulaz je ime i prezime
+		// Izlaz email adresa u Edunovi
+
+		// primjer: Tomislav Jakopec -> tjakopec@edunova.hr
+		
+		String ime = "Jakob Brkić";
+		String[] imePrezime = ime.split(" ");
+		String email="";
+		
+		email = imePrezime[0].substring(0,1).toLowerCase()+imePrezime[1].toLowerCase();
+		System.out.println(email + "@edunova.hr");
+
+	}
+}
