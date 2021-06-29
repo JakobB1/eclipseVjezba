02efb8a9f958742d6b8f8cc020ce9f6132cbd5d5
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-29 18:08:08 +0100

    zadatak03

--- Edunova08/src/edunova/zadaci/Zadatak03.java
+++ Edunova08/src/edunova/zadaci/Zadatak03.java
@@ -5,5 +5,20 @@ public class Zadatak03 {
 	// Kreirajte metodu razlika tipa int koja
 	// prima dva parametra tipa String
 	// Metoda vraæa razliku prvog i drugog primljenog broja
+	
+	private static int razlika(String a, String b) {
+		int a1= Integer.parseInt(a);
+	    int b1= Integer.parseInt(b);
+	    int rez= a1 - b1;
+	    
+	    return rez;
+	}
+	
+	public static void main(String[] args) {
+		
+		int c = Zadatak03.razlika("10", "7");
+		
+		System.out.println(c);
+	}
 
 }
