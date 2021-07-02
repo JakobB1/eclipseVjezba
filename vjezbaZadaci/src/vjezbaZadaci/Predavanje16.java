dca748da67a48e21aeafecd75cf2a2a6dadf8d96
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-02 21:53:58 +0100

    vjezba

--- vjezbaZadaci/src/vjezbaZadaci/Predavanje16.java
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje16.java
@@ -2,16 +2,31 @@ package vjezbaZadaci;
 
 public class Predavanje16 {
 
-	// pomoću while petlje ispišite sve parne brojeve
-	// od 3 do 22 uz iznimku brojeva 8 i 10 (njih ne treba ispisati)
-
 	public static void main(String[] args) {
-			int i=3;
-			while(i<=22) {
-				if(i%2==0 && !(i==8 || i==10)) {
-					System.out.println(i);
-				}
-				i++;
+
+		// Zadatak 1
+		// pomoću while petlje ispišite sve parne brojeve
+		// od 3 do 22 uz iznimku brojeva 8 i 10 (njih ne treba ispisati)
+
+		int i = 3;
+		while (i <= 22) {
+			if (i % 2 == 0 && !(i == 8 || i == 10)) {
+				System.out.println(i);
 			}
+			i++;
+		}
+
+		// Zadatak 2
+		// s pomoću do while petlje zbrojite prvih 100 brojeva
+		// ispišite rezultat
+
+		int j = 0;
+		int sum = 0;
+
+		do {
+			sum += j;
+		} while (++j <= 100);
+
+		System.out.println(sum);
 	}
 }
