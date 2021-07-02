c5fe74c4a530733e71bd17e3002cbfb723a2c3d6
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-02 22:01:44 +0100

    vjezba

--- vjezbaZadaci/src/vjezbaZadaci/Predavanje16.java
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje16.java
@@ -1,10 +1,13 @@
 package vjezbaZadaci;
 
+import java.util.Arrays;
+
 public class Predavanje16 {
 
 	public static void main(String[] args) {
 
 		// Zadatak 1
+
 		// pomoću while petlje ispišite sve parne brojeve
 		// od 3 do 22 uz iznimku brojeva 8 i 10 (njih ne treba ispisati)
 
@@ -17,6 +20,7 @@ public class Predavanje16 {
 		}
 
 		// Zadatak 2
+
 		// s pomoću do while petlje zbrojite prvih 100 brojeva
 		// ispišite rezultat
 
@@ -29,4 +33,38 @@ public class Predavanje16 {
 
 		System.out.println(sum);
 	}
+
+	// Zadatak 3
+
+	// Ulaz u program je niz znakova koji sadrži
+	// imena gradova razdvojeno zarezom
+	// Program ispisuje naziv grada koji u sebi ima najmanje znakova
+
+	// Primjer ulaza: "Osijek, Zagreb, Varaždin, Sinj, Donji Miholjac"
+	// Sinj
+
+	/*
+	 * public static void main(String[] args) { String s =
+	 * "Osijek, Zagreb, Varaždin, Sinjski lug, Donji Miholjac"; String[] gradovi =
+	 * s.split(","); int najmanji=Integer.MAX_VALUE; int index=0; for(int
+	 * i=0;i<gradovi.length;i++) { if(gradovi[i].trim().length()<najmanji) {
+	 * najmanji = gradovi[i].trim().length(); index=i; } }
+	 * System.out.println(gradovi[index].trim().toUpperCase()); for(String grad:
+	 * gradovi) { if(grad.trim().length()==najmanji) {
+	 * System.out.println(grad.trim().toLowerCase()); } }
+	 */
+
+	// Zadatak 4
+
+	// Ulaz je ime i prezime
+	// Izlaz email adresa u Edunovi
+
+	// primjer: Tomislav Jakopec -> tjakopec@edunova.hr
+
+	/*String ime = "Jakob Brkić";
+	String[] imePrezime = ime.split(" ");
+	String email="";
+
+	email=imePrezime[0].substring(0,1).toLowerCase()+imePrezime[1].toLowerCase();System.out.println(email+"@edunova.hr");
+    */
 }
