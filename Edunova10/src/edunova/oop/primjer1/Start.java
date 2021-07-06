683184ff5013172e1157dcb3427fbe693fef9f67
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-06 17:32:16 +0100

    Edunova10

---
+++ Edunova10/src/edunova/oop/primjer1/Start.java
@@ -0,0 +1,12 @@
+package edunova.oop.primjer1;
+
+public class Start {
+	
+	// Klasa Dokument ima svojstva: ID, iznos, datum
+	// Klasa Račun nasljeđuje Dokument i dodaje svojstvo: Kupac
+	// Kupac ima ID, naziv i adresu
+	// Klasa Primka nasljeđuje Dokument i dodaje svojstvo: otpremnica
+	
+	// Kreirati po jednu instancu klasa Račun i Primka
+
+}
