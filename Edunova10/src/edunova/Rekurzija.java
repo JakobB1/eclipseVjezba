f63d5a9e9322d15dab20593ff9d668082d796121
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-06 13:53:08 +0100

    Edunova10

---
+++ Edunova10/src/edunova/Rekurzija.java
@@ -0,0 +1,32 @@
+package edunova;
+
+public class Rekurzija {
+	
+	public Rekurzija() {
+		
+		//zbrajanje prvih 100 brojeva
+		int zbroj=0;
+		for(int i=0;i<=100;i++) {
+			zbroj+=i;
+		}
+		System.out.println(zbroj);
+		
+		System.out.println(suma(100));
+	
+	}
+	
+	private int suma(int broj) {
+		// ključna stvar rekurzije: Uvjet izlazka iz rekurzije
+		// izlazi se kada više ne zoveš samog sebe
+		if(broj==0) {
+			return 0;
+		}
+		//funkcija zove samu sebe - REKURZIJA
+		return broj + suma(broj-1);
+	}
+	// 100 + 99 + .... + 1 + 0;
+
+	public static void main(String[] args) {
+		new Rekurzija();
+	}
+}
