f78f59c7ffdbad5a7eb87c5db33088d3bef8bde7
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-09 12:41:35 +0100

    Edunova11

--- Edunova11/src/edunova/RadSDatumima.java
+++ Edunova11/src/edunova/RadSDatumima.java
@@ -1,5 +1,7 @@
 package edunova;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -12,12 +14,31 @@ public class RadSDatumima {
 		
 		Calendar gc = GregorianCalendar.getInstance();
 		gc.set(Calendar.YEAR, 2021);
-		gc.set(Calendar.MONTH, 11);  // siječanj je 0
+		gc.set(Calendar.MONTH, 10);  // siječanj je 0
 		gc.set(Calendar.DAY_OF_MONTH, 15);
 		datumRodenja = gc.getTime();
 		
 		System.out.println(datumRodenja);
+		// dokumentacija
+		SimpleDateFormat df = new SimpleDateFormat("dd. MM. yyyy.");
+		System.out.println(df.format(datumRodenja));
 		
+		try {
+			datumRodenja = df.parse("16. 06. 2020.");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(df.format(datumRodenja));
+		// dokumentacija
+		// dokumentacija
+		System.out.println(datumRodenja.getTime());
+		
+		gc.setTime(datumRodenja);
+		gc.add(Calendar.MONTH, 45);
+		
+		System.out.println(df.format(gc.getTime()));
 	}
 	
 	public static void main(String[] args) {
