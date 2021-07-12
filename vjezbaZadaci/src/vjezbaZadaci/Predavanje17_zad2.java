1277ee69d603f0351f312371991b9215d2eefaf6
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-12 16:46:47 +0100

    zadatak02

---
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje17_zad2.java
@@ -0,0 +1,26 @@
+package vjezbaZadaci;
+
+import java.util.Arrays;
+
+public class Predavanje17_zad2 {
+	
+	// Zadatak 2
+	// Kreirajte metodu slucajniBrojevi 
+	// tipa niz decimalnih brojeva (double)
+	// koja vraæa 10 sluèajnih brojeva
+	// Math.random();
+
+
+	private static double[] slucajniBrojevi() {
+		double[] brojevi = new double[10];
+		for(int i=0;i<10;i++) {
+			brojevi[i]=Math.random();
+		}
+		return brojevi;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(slucajniBrojevi()));
+	}
+
+}
