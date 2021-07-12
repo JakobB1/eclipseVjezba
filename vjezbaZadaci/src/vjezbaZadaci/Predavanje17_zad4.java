0a75b3e7e97e65ce2ba492f695850bf1c2c31159
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-12 16:52:24 +0100

    zadatak04

---
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje17_zad4.java
@@ -0,0 +1,26 @@
+package vjezbaZadaci;
+
+public class Predavanje17_zad4 {
+	
+	// Zadatak 4
+	// Kreirajte metodu aritmetièkas sredina tipa float
+	// metoda prima niz cijelih brojeva
+	// metoda vraæa aritmetièku sredinu svih primljenih brojeva
+	
+	private static float aritmetickaSredina(int[]niz) {
+		int zbroj=0;
+		
+		for(int i=0; i<niz.length; i++) {
+			zbroj += niz[i];
+		}
+		
+		return (float) zbroj/niz.length;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] pocetniNiz = {5,4,2,5,8,3,5,42};
+		System.out.println(aritmetickaSredina(pocetniNiz));
+	}
+
+}
