316f0bdfd9847ca9d75d0f6220b3aabb4d7c9f22
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-12 16:00:16 +0100

    zadatak01

---
+++ vjezbaZadaci/src/vjezbaZadaci/Predavanje17.java
@@ -0,0 +1,23 @@
+package vjezbaZadaci;
+
+public class Predavanje17 {
+	
+	// Zadatak 1
+	// Kreirati metodu tipa int naziva suma 
+	// koja prima niz cijelih brojeva
+	// metoda vraæa sumu svih brojeva u nizu
+	
+	// Metodu pozvati iz main metode
+
+	private static int suma(int[] brojevi) {
+		int suma=0;
+		for(int i: brojevi) {
+			suma+=i;
+		}
+		return suma;
+	}
+	public static void main(String[] args) {
+		int[] niz = {3,4,2,4,4,2,1};
+		System.out.println(suma(niz));
+	}
+}
