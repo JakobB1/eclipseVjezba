434f01e7c15a57b133258fedf432d4915bdf82cb
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-13 16:54:17 +0100

    Edunova12

--- Edunova12/src/edunova/Start.java
+++ Edunova12/src/edunova/Start.java
@@ -1,15 +1,28 @@
 package edunova;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Start {
 	
 	private List<Smjer> smjerovi;
  	
 	public Start() {
-	
+		smjerovi = new ArrayList<>();
+		izbornik();
 	}
 	
+	private void izbornik() {
+		System.out.println("******** Edunova APP konzola ********");
+		System.out.println("IZBORNIK");
+		System.out.println(" 1. Smjerovi");
+		System.out.println(" 2. Polaznici");
+		System.out.println(" 3. Predavači");
+		System.out.println(" 4. Grupe");
+		System.out.println(" 5. Izlazi iz programa");		
+	}
+
 	public static void main(String[] args) {
 		new Start();
 	}
